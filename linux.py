e94a104e84e275ebabc5f5f75b954c3525d4e3a8
striter-no <ivansalasugin@gmail.com>
2025-03-17 07:11:43 +0300

    Updated structure and system prompt

--- linux.py
+++ linux.py
@@ -1,7 +1,7 @@
-from gpt import *
+from src.gpt import *
 import json as jn
 
-config = jn.load(open('gpt_conf.json'))
+config = jn.load(open('./configs/gpt_conf.json'))
 chat = Chat(
     model=models_stock.claude_3_5_sonnet,
     provider=provider_stock.PollinationsAI
